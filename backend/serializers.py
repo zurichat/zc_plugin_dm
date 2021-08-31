69237b356646627ec33b0206ff97762a9b2b8b4e
unknown <dfelastevegit@gmail.com>
2021-08-31 23:51:36 +0100

    Create serializer class for User #152

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,1 +1,9 @@
-pass
+from .models import User
+from rest_framework import serializers
+
+
+# User serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
