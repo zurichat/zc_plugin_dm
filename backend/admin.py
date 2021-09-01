c68ce2fb54e39fcf6628ca395de618fa312c2b2e
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-09-01 12:30:12 +0100

    Made Book_Mark Model

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import book_mark
 
 # Register your models here.
+admin.site.register(book_mark)
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class book_mark(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    link = models.CharField(max_length=900)
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.user
+
