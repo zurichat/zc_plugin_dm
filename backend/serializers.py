a56080e3b78625d4cb72ab6018450e9692785739
unknown <dfelastevegit@gmail.com>
2021-09-01 23:31:21 +0100

    user serializer

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,9 +1,9 @@
-from .models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
 # User serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = '__all__'
