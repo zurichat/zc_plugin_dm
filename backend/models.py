b48b9d47e05741a1f508fe466c38a975db7a8e08
unknown <dfelastevegit@gmail.com>
2021-09-02 13:25:44 +0100

    setup for task

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.contrib.auth.model import User
+
 
 
 class book_mark(models.Model):
-    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete= models.CASCADE)
     link = models.CharField(max_length=900)
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
 
