204b9eea3cee37fc7fba3d325e96798f3ba23d5e
xzenon <forlanfr_8@hotmail.fr>
2021-09-03 20:51:43 +0100

    New commit

--- prod_build.sh
+++ prod_build.sh
@@ -3,7 +3,7 @@
 #static files route set to remote host
 #change this to your remote host address + /static
 #example: https://dm.zuri.chat/static
-export BASE_URL="https://dm.zuri.chat/static"
+export BASE_URL="http://localhost/static"
 
 #change to frontend directory and build app with npm
 cd frontend
