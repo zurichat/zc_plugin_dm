20388f6ec0449aa547ffb448245ab738dd43c8cd
DevOpes <65594389+Oprahjosh@users.noreply.github.com>
2021-09-03 19:38:57 +0100

    issue_resolved

--- backend/views.py
+++ backend/views.py
@@ -7,7 +7,6 @@ from rest_framework.decorators import api_view
 from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework import status
-from .models import User
 
 # Create your views here.
 
