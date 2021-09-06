9bfac4d2bc07a432fba119b27fd34af560a0c3ae
Delight Fela-Steve <69828185+Delight-Fela-Steve@users.noreply.github.com>
2021-09-06 15:01:56 +0100

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -39,7 +39,7 @@ urlpatterns = [
     path('api/getstarred', views.get_starred, name="get_starred"),
     path('api/sendFile', views.send_file),
     path('api/sendMedia', views.send_media),
-    path('api/messages', views.messages_list),
+    path('api/messages', views.message_list),
     path('api/starMessage', views.star_messages),
     path('api/messagesByDate', views.index),
     path('api/messageByUser', views.filter_user),
