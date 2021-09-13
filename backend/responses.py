37698e3086efa1846088f91eb57ed9ba64922b5d
mykie88 <mykhelmyers@gmail.com>
2021-09-13 18:52:07 +0100

    updated decorators & added message response field

--- backend/responses.py
+++ backend/responses.py
@@ -10,7 +10,10 @@ class RoomInfoResponse(serializers.Serializer):
 
 
 class MessageResponse(serializers.Serializer):
-    pass
+    status = serializers.CharField(read_only=True)
+    message_id = serializers.ReadOnlyField()
+    data = serializers.DictField(child=serializers.CharField(), read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
 
 
 class CreateRoomResponse(serializers.Serializer):
