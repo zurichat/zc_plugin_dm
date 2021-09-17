6d2a4282e7f498c826a7e29b17b40f3854ec20e5
xzenon <forlanfr_8@hotmail.fr>
2021-09-17 14:17:48 +0100

    Remove Centrifugo settings

--- zc_plugin_dm/settings.py
+++ zc_plugin_dm/settings.py
@@ -153,8 +153,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-# CENTRIFUGO settings
-
-CENTRIFUGO_HOST = 'https://realtime.zuri.chat/api'
-CENTRIFUGO_API_TOKEN = '58c2400b-831d-411d-8fe8-31b6e337738b'
-
