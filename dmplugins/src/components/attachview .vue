53630e455494b491ab86672596aa88627c33b66f
Ayomide26 <ayomidefadare26@gmail.com>
2021-09-20 20:18:35 +0100

    attach file

---
+++ dmplugins/src/components/attachview .vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+       
+      <AttachFile class="ease"  v-show='toggle'/>
+ <button class="btn btn-primary m-5" ><b-icon icon="paperclip" @click='toggle = !toggle'></b-icon></button>
+  </div>
+</template>
+
+<script>
+import AttachFile from '@/components/attachFile.vue'
+export default {
+   data() {
+    return { 
+     toggle: true,
+    
+    };
+  },
+components:{
+    AttachFile,
+}
+}
+</script>
+
+<style scoped>
+.ease{
+      transition: 0.5s ease-out;
+
+}
+
+button{
+ position:fixed;
+  right:2px;
+  bottom:2px;
+}
+</style>
