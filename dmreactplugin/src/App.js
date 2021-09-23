6708862162c93c0a1f3d6076045b4652d5037b30
abisalde <abisalde>
2021-09-23 15:48:06 -0400

    reviews

--- dmreactplugin/src/App.js
+++ dmreactplugin/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Import all Global CSS components
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/assets/css/global.module.css';
 
 // Import all Router components
 import ChatHome from './pages/chathome';
