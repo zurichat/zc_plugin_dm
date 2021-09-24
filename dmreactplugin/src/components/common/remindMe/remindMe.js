e7d8ae51dc48d3ca66f8d0eef6d9c1d753f88648
Mogaka Amos N <mogaka.amo254@gmail.com>
2021-09-24 12:48:23 +0000

    Update remindMe.js

--- dmreactplugin/src/components/common/remindMe/remindMe.js
+++ dmreactplugin/src/components/common/remindMe/remindMe.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { DatePicker, TimePicker } from "react-rainbow-components";
+import './RemindMe.css'
 
 const RemindMe = () => {
-    return (
-        <div>
-            <h1>Hello remind me</h1>     
-        </div>
-    )
-}
+  const [date, setDate] = useState(null);
 
-export default RemindMe
-        
+  function onChange(date) {
+    setDate(date);
+  }
+  return (
+    <div className="remind-me-wrapper">
+      <Form className="remind-me">
+        <h3 className="remind-me-header">Create a reminder</h3>
+        <FormGroup className="date-wrapper">
+          <Label for="date">Date</Label>
+          <DatePicker
+            id="datePicker-1"
+            value={date}
+            onChange={onChange}
+            label=""
+            formatStyle="large"
+          />
+        </FormGroup>
+        <FormGroup className="time-wrapper">
+          <Label for="time">Time</Label>
+          <TimePicker
+            id="datePicker-1"
+            value={date}
+            onChange={onChange}
+            label=""
+            formatStyle="large"
+          />
+        </FormGroup>
+        <FormGroup className="note-wrapper">
+          <Label for="note">
+            Add a note<em>(optional)</em>
+          </Label>
+          <Input type="textarea" name="text" id="exampleText" />
+        </FormGroup>
+        <FormGroup className="button-wrapper">
+          <Button className="cancel">Cancel</Button>
+          <Button className="create">Create</Button>
+        </FormGroup>
+      </Form>
+    </div>
+  );
+};
+
+export default RemindMe;
