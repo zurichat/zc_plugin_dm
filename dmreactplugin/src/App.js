cfb358d053aa26aecfe46ab5495ddae29168de98
kanikamital0606 <kanikamittal0661996@gmail.com>
2021-09-25 04:47:43 +0530

    msg thread

--- dmreactplugin/src/App.js
+++ dmreactplugin/src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 // Import all Global CSS components
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/assets/css/global.module.css';
 
 // Import all Router components
 import ChatHome from './pages/chathome';
-
+import dmSingleMessageContainer from './components/dmSingleMessageContainer';
 const App = () => {
     return (
         <Router basename='/dm'>
             <Switch>
                 <Route exact path='/' component={ChatHome} />
+                <Route exact path='/message' component={dmSingleMessageContainer} />
             </Switch>
         </Router>
     );
