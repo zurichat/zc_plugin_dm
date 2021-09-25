17b5ece99622be2b2dbf338732917a5525efb71a
StartUPJahswill <princejahsman@gmail.com>
2021-09-25 17:48:59 +0100

    I created and styled the ThreeDotKebabMenu and Submenu

--- dmreactplugin/src/pages/chathome.js
+++ dmreactplugin/src/pages/chathome.js
@@ -1,10 +1,12 @@
 import React from "react";
 import DmFullProfile from "../components/DmFullProfile/DmFullProfile";
+import ThreeDotKebab from "../components/common/threeDotKebabMenu/threeDotKebab";
 
 const ChatHome = () => {
   return (
     <div className="chat-home">
       <DmFullProfile />
+      <ThreeDotKebab />
     </div>
   );
 };
