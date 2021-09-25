4b490eb5788fd4ceec83a1f36fbd0a5664ff4e04
abisalde <abisalde>
2021-09-25 11:53:38 -0400

    header refactor

--- dmreactplugin/src/pages/chathome.js
+++ dmreactplugin/src/pages/chathome.js
@@ -1,12 +1,13 @@
-import React from "react";
-import DmFullProfile from "../components/DmFullProfile/DmFullProfile";
+import React from 'react';
+import DmFullProfile from '../components/DmFullProfile/DmFullProfile';
+import DmProfileHeader from '../components/dmProfileHeader';
 
 const ChatHome = () => {
-  return (
-    <div className="chat-home">
-      <DmFullProfile />
-    </div>
-  );
+    return (
+        <div className='chat-home'>
+            <DmProfileHeader />
+        </div>
+    );
 };
 
 export default ChatHome;
