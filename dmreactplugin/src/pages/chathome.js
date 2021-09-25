4a509899ca48483e60dc8920c056f79d5876ce14
Beke Ijobra <ijobra20@gmail.com>
2021-09-25 06:58:41 +0100

    final resolve

--- dmreactplugin/src/pages/chathome.js
+++ dmreactplugin/src/pages/chathome.js
@@ -1,11 +1,9 @@
 import React from "react";
 import DmFullProfile from "../components/DmFullProfile/DmFullProfile";
-import ChatHome from "./ChatHome";
 
 const ChatHome = () => {
   return (
     <div className="chat-home">
-      <ChatHome />
       <DmFullProfile />
     </div>
   );
