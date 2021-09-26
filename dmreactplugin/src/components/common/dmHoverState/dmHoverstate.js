13c005f222960e1321d750df44f8bbf164f1fa7c
Richardjackson-web <richardjackson556@gmail.com>
2021-09-26 21:18:47 +0100

    Working on pixel perfection and adjusted styling names.

--- dmreactplugin/src/components/common/dmHoverState/dmHoverstate.js
+++ dmreactplugin/src/components/common/dmHoverState/dmHoverstate.js
@@ -1,40 +1,39 @@
 import React, { useState } from "react";
-import '../dmHoverState/dmHoverstate.css';
-import { BiSmile } from 'react-icons/bi';
-import { BsFillChatDotsFill } from 'react-icons/bs';
+import './../src/App.css';
+import { CgSmile } from "react-icons/cg";
+import { BsChatDots } from 'react-icons/bs';
 import { FiShare2 } from 'react-icons/fi';
-import { GiSaveArrow } from 'react-icons/gi';
-import { AiOutlineMore } from 'react-icons/ai';
+import { CgBookmark } from 'react-icons/cg';
+import { CgMoreVertical } from 'react-icons/cg';
 import ReactTooltip from 'react-tooltip'
 
 
-function dmHoverState() {
-    const [isShown, setIsShown] = useState(false);
-  
-    return (
-      <div className="dmHover">
-  
-        {isShown && (
-          <div>
-           <ul  className="dmIcons">
-             <li><BiSmile ref={ref => this.fooRef = ref} data-tip='Add reaction'/></li>
-             <li><BsFillChatDotsFill ref={ref => this.fooRef = ref} data-tip='Reply in thread'/></li>
-             <li><FiShare2 ref={ref => this.fooRef = ref} data-tip='Share message...'/></li>
-             <li><GiSaveArrow ref={ref => this.fooRef = ref} data-tip='Add to saved items'/></li>
-             <li><AiOutlineMore ref={ref => this.fooRef = ref} data-tip='More actions'/></li>
-           </ul>
-           <ReactTooltip />
-          </div>
-        )}
-        <span  className="dmState"
-          onMouseEnter={() => setIsShown(true)}
-          onMouseLeave={() => setIsShown(true)}>
-          Sample Message Thread
-        </span>
-        
-      </div>
-    );
-  }
-  
-  export default dmHoverState;
-  
+function dmHoverstate() {
+  const [isShown, setIsShown] = useState(false);
+
+  return (
+    <div className="dmHover">
+
+      {isShown && (
+        <div className="dmCover">
+         <ul  className="dmIcons">
+           <li className="dmLi"><CgSmile ref={ref => this.fooRef = ref} data-tip='Add reaction'/></li>
+           <li className="dmLi"><BsChatDots ref={ref => this.fooRef = ref} data-tip='Reply in thread'/></li>
+           <li className="dmLi"><FiShare2 ref={ref => this.fooRef = ref} data-tip='Share message...'/></li>
+           <li className="dmLi"><CgBookmark ref={ref => this.fooRef = ref} data-tip='Add to saved items'/></li>
+           <li className="dmLi"><CgMoreVertical ref={ref => this.fooRef = ref} data-tip='More actions'/></li>
+         </ul>
+         <ReactTooltip />
+        </div>
+      )}
+      <span  className="dmState"
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(true)}>
+        Sample Message Thread
+      </span>
+      
+    </div>
+  );
+}
+
+export default dmHoverstate;
