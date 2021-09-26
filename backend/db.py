220f188f1fd47db5ce16342220f41fcee22547a7
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2021-09-26 22:02:53 +0100

    nawa

--- backend/db.py
+++ backend/db.py
@@ -205,7 +205,7 @@ def get_rooms(user_id, org_id):
             except Exception:
                 pass
         if len(data) == 0:
-            data = None
+            data = []
             return data
         return data
     
