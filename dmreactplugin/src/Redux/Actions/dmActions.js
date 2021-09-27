413a87fe630c3fda0a08922301b680353659cf7d
Godswill Edet <gimmex1@gmail.com>
2021-09-27 16:08:49 +0100

    Added the getRooms actions and reducer in redux

--- dmreactplugin/src/Redux/Actions/dmActions.js
+++ dmreactplugin/src/Redux/Actions/dmActions.js
@@ -1,49 +1,60 @@
 // Import all API endpoints
-import APIService from '../../utils/apiServices';
+import axios from "axios";
+import APIService from "../../utils/apiServices";
 
 // import all action types
 import {
-    GET_MEMBERS,
-    CREATE_ROOM,
-    GET_ROOMS,
-    GET_SOCKETS,
-    GET_MESSAGES,
-    SEND_MESSAGE,
-    GET_PINNED_MESSAGE,
-    MESSAGE_REACTION,
-} from './actionTypes';
+  GET_MEMBERS,
+  CREATE_ROOM,
+  GET_ROOMS,
+  GET_SOCKETS,
+  GET_MESSAGES,
+  SEND_MESSAGE,
+  GET_PINNED_MESSAGE,
+  MESSAGE_REACTION,
+} from "./actionTypes";
 
 // Get Organization Members
-const handleGetMembers = (org_id) => async (dispatch) => {
+export const handleGetMembers = (org_id) => async (dispatch) => {
+  try {
+    const res = await APIService.getOrgUsers(org_id);
+
+    dispatch({ type: GET_MEMBERS, payload: res.data.data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createDmRoom =
+  (org_id, user1_id, user2_id) => async (dispatch) => {
     try {
-        const res = await APIService.getOrgUsers(org_id);
+      const res = await APIService.createChatRoom(org_id, {
+        data: {
+          org_id: org_id,
+          room_user_ids: [user1_id, user2_id],
+          bookmark: ["string"],
+          pinned: ["string"],
+        },
+      });
 
-        dispatch({ type: GET_MEMBERS, payload: res.data.data });
+      dispatch({ type: CREATE_ROOM, payload: res.data.data });
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
+  };
+
+// Get rooms
+
+const getRooms = (rooms) => ({
+  type: GET_ROOMS,
+  payload: rooms,
+});
+
+export const handleGetRooms = (org_id, user_id) => async (dispatch) => {
+  try {
+    const { data } = await APIService.getRoomUserId(org_id, user_id);
+    dispatch(getRooms(data));
+  } catch (error) {
+    console.log(`Error from handleGetROoms: ${error}`);
+  }
 };
-
-const createDmRoom = (org_id, user1_id, user2_id) => async (dispatch) => {
-    try {
-        const res = await APIService.createChatRoom(org_id, {
-            data: {
-                org_id: org_id,
-                room_user_ids: [user1_id, user2_id],
-                bookmark: ['string'],
-                pinned: ['string'],
-            },
-        });
-
-        dispatch({ type: CREATE_ROOM, payload: res.data.data });
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-const appActions = {
-    handleGetMembers,
-    createDmRoom,
-};
-
-export default appActions;
