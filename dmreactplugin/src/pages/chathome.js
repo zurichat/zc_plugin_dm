bd66a53adca3bbf65615befa0f2b5c8aad808431
StartUPJahswill <princejahsman@gmail.com>
2021-09-27 13:29:02 +0100

    I created and styled the ThreeDotKebab Popup Menu and Submenu for the Hover State. I also added a button function to test since the Hover state is not ready

--- dmreactplugin/src/pages/chathome.js
+++ dmreactplugin/src/pages/chathome.js
@@ -1,10 +1,13 @@
 import React from "react";
+import ThreeDotKebab from "../components/common/threeDotKebabMenu/threeDotKebab";
 import DmFullProfile from "../components/DmFullProfile/DmFullProfile";
 
 const ChatHome = () => {
   return (
     <div className="chat-home">
       <DmFullProfile />
+      {/* This is the popup menu for the threeDotKebab hover state component */}
+      <ThreeDotKebab />
     </div>
   );
 };
