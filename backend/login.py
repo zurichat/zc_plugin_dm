b2a8faf5709608fe5ebf55bdbca9e9c7bb3c7696
Psami-wondah <okechukwusamuel16@gmail.com>
2021-09-28 18:05:51 +0100

    Caught exceptions at login.py, added auth to my messagemedia endpoint and the upload methods

--- backend/login.py
+++ backend/login.py
@@ -10,4 +10,7 @@ def login_user():
         response = requests.post(url="https://api.zuri.chat/auth/login", json=data)
     except requests.exceptions.RequestException as e:
         return None
-    return response.json()["data"]["user"]["token"]
+    if response.status_code==200:
+        return response.json()["data"]["user"]["token"]
+    else:
+        return None
