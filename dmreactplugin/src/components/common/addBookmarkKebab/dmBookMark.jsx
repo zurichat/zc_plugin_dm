75d69699fb3c8d75543b5f9bb5db808c57e45faf
kenechukwu offokansi <koffokansi@gmail.com>
2021-09-29 07:03:55 +0100

    removed all console log

--- dmreactplugin/src/components/common/addBookmarkKebab/dmBookMark.jsx
+++ dmreactplugin/src/components/common/addBookmarkKebab/dmBookMark.jsx
@@ -3,7 +3,7 @@ import { BiPlus } from "react-icons/bi";
 import "./dmBookmarkStyle.css";
 import AddBookmarkDropDown from "./dmBookmarkDropDown";
 {
-  /* <div className='addBookmark-overlay'>
+    /* <div className='addBookmark-overlay'>
                 <div className='container mx-auto pt-3 pb-3 addBookmark'>
                     <div className='row d-flex pt-2 pb-2 mb-3 bookmark-link'>
                         <div className='col-auto align-self-center link-btn'>
@@ -23,47 +23,46 @@ import AddBookmarkDropDown from "./dmBookmarkDropDown";
 }
 
 const DmBookMarkHeader = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const HoverOutStop = useRef(false);
-  const onHoverMouseOut = () => {
-    if (HoverOutStop.current) {
-    } else {
-      console.log(HoverOutStop.current);
-      setIsOpen(false);
-    }
-  };
-  return (
-    <>
-      <button
-        className="position-relative btn btn-add-bookmark d-flex align-items"
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-        onMouseLeave={onHoverMouseOut}
-      >
-        <BiPlus />
-        <p className="m-0">Add a bookmark</p>
-        {isOpen ? (
-          <div
-            className="position-absolute bg-white shadow-sm p-3 dropDown-zindex d-flex flex-column gap-2"
-            style={{ width: "400px", top: "100%", left: "0" }}
-            aria-hidden
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-          >
-            <AddBookmarkDropDown
-              onOpenModal={(value) => {
-                HoverOutStop.current = value;
-              }}
-            />
-            <div className="border-bottom border-secondary"></div>
-            <p className="pl-3 text-start">No recent links</p>
-          </div>
-        ) : null}
-      </button>
-    </>
-  );
+    const [isOpen, setIsOpen] = useState(false);
+    const HoverOutStop = useRef(false);
+    const onHoverMouseOut = () => {
+        if (HoverOutStop.current) {
+        } else {
+            setIsOpen(false);
+        }
+    };
+    return (
+        <>
+            <button
+                className="position-relative btn btn-add-bookmark d-flex align-items"
+                onClick={() => {
+                    setIsOpen(!isOpen);
+                }}
+                onMouseLeave={onHoverMouseOut}
+            >
+                <BiPlus />
+                <p className="m-0">Add a bookmark</p>
+                {isOpen ? (
+                    <div
+                        className="position-absolute bg-white shadow-sm p-3 dropDown-zindex d-flex flex-column gap-2"
+                        style={{ width: "400px", top: "100%", left: "0" }}
+                        aria-hidden
+                        onClick={(e) => {
+                            e.stopPropagation();
+                        }}
+                    >
+                        <AddBookmarkDropDown
+                            onOpenModal={(value) => {
+                                HoverOutStop.current = value;
+                            }}
+                        />
+                        <div className="border-bottom border-secondary"></div>
+                        <p className="pl-3 text-start">No recent links</p>
+                    </div>
+                ) : null}
+            </button>
+        </>
+    );
 };
 
 export default DmBookMarkHeader;
