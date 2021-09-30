dd2a519dddac8b3ece850b4e4c62ce50f82b8494
Nwankwo Henry <nameisifunanya@gmail.com>
2021-09-30 09:10:29 +0100

    Sorted the messages relative to time and date

--- dmreactplugin/src/components/ChatContainer/dmChatContainerBox.jsx
+++ dmreactplugin/src/components/ChatContainer/dmChatContainerBox.jsx
@@ -8,44 +8,48 @@ import DmReplyInThread from '../ReplyInThread/replyInThread';
 import './chatContainerBox.css';
 
 const DmChatContainerBox = ({ user2_id }) => {
-    const { room_messages } = useSelector(({ roomsReducer }) => roomsReducer);
-    const [openThread, setOpenThread] = useState(false);
+  const { room_messages } = useSelector(({ roomsReducer }) => roomsReducer);
+  const [openThread, setOpenThread] = useState(false);
 
-    const handleOpenThread = () => {
-        setOpenThread(true);
-    };
+  const handleOpenThread = () => {
+    setOpenThread(true);
+  };
 
-    const handleCloseThread = () => {
-        setOpenThread(false);
-    };
+  const handleCloseThread = () => {
+    setOpenThread(false);
+  };
 
-    return (
-        <>
-            <div className='dm-chatContainerBox w-100 d-flex align-items-end'>
-                <main className='dm-chat-main-container'>
-                    <DmInitMessageBox />
+  return (
+    <>
+      <div className='dm-chatContainerBox w-100 d-flex align-items-end'>
+        <main className='dm-chat-main-container'>
+          <DmInitMessageBox />
 
-                    {room_messages?.results
-                        ? room_messages?.results?.map((messages) => (
-                              <div key={messages?.id}>
-                                  <MessageWrapper
-                                      handleOpenThread={handleOpenThread}
-                                  >
-                                      <DmSingleMessageContainer
-                                          messages={messages}
-                                          user2_id={user2_id}
-                                      />
-                                  </MessageWrapper>
-                              </div>
-                          ))
-                        : null}
-                </main>
-                <aside className={`asideContent ${openThread ? 'active' : ''}`}>
-                    <DmReplyInThread handleCloseThread={handleCloseThread} />
-                </aside>
-            </div>
-        </>
-    );
+          {room_messages?.results
+            ? room_messages?.results
+                ?.sort((first, second) => {
+                  const firstDate = new Date(first.created_at);
+                  const secondDate = new Date(second.created_at);
+                  return firstDate - secondDate;
+                })
+                .map((messages) => (
+                  <div key={messages?.id}>
+                    <MessageWrapper handleOpenThread={handleOpenThread}>
+                      <DmSingleMessageContainer
+                        messages={messages}
+                        user2_id={user2_id}
+                      />
+                    </MessageWrapper>
+                  </div>
+                ))
+            : null}
+        </main>
+        <aside className={`asideContent ${openThread ? 'active' : ''}`}>
+          <DmReplyInThread handleCloseThread={handleCloseThread} />
+        </aside>
+      </div>
+    </>
+  );
 };
 
 export default DmChatContainerBox;
