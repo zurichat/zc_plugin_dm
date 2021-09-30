87a1227753c1a8e3ac104985808f7ed6bd812742
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2021-10-01 00:44:36 +0100

    removed db decorator and added thread update to thread detail class

--- backend/urls.py
+++ backend/urls.py
@@ -17,11 +17,6 @@ urlpatterns = [
         views.search_DM,
         name="search DM",
     ),
-    path(
-        "api/v1/org/<str:org_id>/rooms/<str:room_id>/messages/<str:message_id>/threads/<str:message_uuid>",
-        views.update_thread_message,
-        name="update_thread_message",
-    ),
     path("api/v1/org/<str:org_id>/room", views.create_room, name="create_room"),
     # path(
     #     "api/v1/org/<str:org_id>/rooms/<str:room_id>/messages/<str:message_id>/threads",
