310a5b0f71a721a60be8e20019cc52381fb86eea
fas3a <fas3a4real@gmail.com>
2021-10-02 23:47:58 +0100

    Implementation of Bookmark

--- dmreactplugin/src/components/common/addBookmarkKebab/dmBookMark.jsx
+++ dmreactplugin/src/components/common/addBookmarkKebab/dmBookMark.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { BiPlus } from "react-icons/bi";
 import "./dmBookmarkStyle.css";
 import AddBookmarkDropDown from "./dmBookmarkDropDown";
+
+
 {
-  /* <div className='addBookmark-overlay'>
+    /* <div className='addBookmark-overlay'>
                 <div className='container mx-auto pt-3 pb-3 addBookmark'>
                     <div className='row d-flex pt-2 pb-2 mb-3 bookmark-link'>
                         <div className='col-auto align-self-center link-btn'>
@@ -23,46 +25,81 @@ import AddBookmarkDropDown from "./dmBookmarkDropDown";
 }
 
 const DmBookMarkHeader = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const HoverOutStop = useRef(false);
-  const onHoverMouseOut = () => {
-    if (HoverOutStop.current) {
-    } else {
-      setIsOpen(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const [bookmarks, setBookmark] = useState([]);
+    function getBookMark() {
+        fetch("https://dm.zuri.chat/api/v1/org/614679ee1a5607b13c00bcb7/rooms/615110993036822db275f738/bookmarks")
+            .then(data => data.json())
+            .then(data => {
+                console.log(data);
+                setBookmark(data);
+            });
     }
-  };
-  return (
-    <>
-      <button
-        className="position-relative btn btn-add-bookmark d-flex align-items"
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-        onMouseLeave={onHoverMouseOut}
-      >
-        <BiPlus />
-        <p className="m-0">Add a bookmark</p>
-        {isOpen ? (
-          <div
-            className="position-absolute bg-white shadow-sm p-3 dropDown-zindex d-flex flex-column gap-2"
-            style={{ width: "400px", top: "100%", left: "0" }}
-            aria-hidden
-            onClick={(e) => {
-              e.stopPropagation();
+
+    useEffect(() => {
+        getBookMark();
+    }, []);
+
+
+    const HoverOutStop = useRef(false);
+    const onHoverMouseOut = () => {
+        if (HoverOutStop.current) {
+        } else {
+            setIsOpen(false);
+        }
+    };
+    function renderBookmark() {
+        var returnHTML = "";
+        if (bookmarks.length > 0) {
+            returnHTML = bookmarks.map((bookmark) => {
+                return (
+                    <div className="display-bookmark" key={name}>
+                        <h3 className="display-bookmark-link">{bookmark.link}</h3>
+                        <h4 className="display-bookmark-name">{bookmark.name} - {bookmark.created_at} </h4>
+                    </div>
+                )
+
+            })
+        } else {
+            returnHTML = < p className="pl-3 text-start" > No recent links</p >;
+        }
+
+        return returnHTML;
+    }
+
+    return (
+        <>
+        <button
+            className="position-relative btn btn-add-bookmark d-flex align-items"
+            onClick={() => {
+                setIsOpen(!isOpen);
             }}
-          >
-            <AddBookmarkDropDown
-              onOpenModal={(value) => {
-                HoverOutStop.current = value;
-              }}
-            />
-            <div className="border-bottom border-secondary"></div>
-            <p className="pl-3 text-start">No recent links</p>
-          </div>
-        ) : null}
-      </button>
-    </>
-  );
+            onMouseLeave={onHoverMouseOut}
+        >
+            <BiPlus />
+            <p className="m-0">Add a bookmark</p>
+            {isOpen ? (
+                <div
+                    className="position-absolute bg-white shadow-sm p-3 dropDown-zindex d-flex flex-column gap-2"
+                    style={{ width: "400px", top: "100%", left: "0" }}
+                    aria-hidden
+                    onClick={(e) => {
+                        e.stopPropagation();
+                    }}
+                >
+                    <AddBookmarkDropDown
+                        onOpenModal={(value) => {
+                            HoverOutStop.current = value;
+                        }}
+                    />
+                    <div className="border-bottom border-secondary">
+                    </div>
+                    {renderBookmark()}
+                </div>
+            ) : null}
+        </button>
+        </>
+    );
 };
 
 export default DmBookMarkHeader;
