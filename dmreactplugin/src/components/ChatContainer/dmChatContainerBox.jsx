2d24b52d3b15eac9ca95688a3dc5f382f210a8be
Wealth Iduwe <wealthiduwe@gmail.com>
2021-10-04 23:17:57 +0100

    modified

--- dmreactplugin/src/components/ChatContainer/dmChatContainerBox.jsx
+++ dmreactplugin/src/components/ChatContainer/dmChatContainerBox.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import MessageWrapper from '../common/dmHoverState/dmHoverstate';
-import DmInitMessageBox from '../dmInitMessagebox';
-import DmSingleMessageContainer from '../dmSingleMessageContainer';
-import DmReplyInThread from '../ReplyInThread/replyInThread';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import MessageWrapper from "../common/dmHoverState/dmHoverstate";
+import DmInitMessageBox from "../dmInitMessagebox";
+import DmSingleMessageContainer from "../dmSingleMessageContainer";
+import DmReplyInThread from "../ReplyInThread/replyInThread";
 
-import './chatContainerBox.css';
+import "./chatContainerBox.css";
+import { FaAngleDown } from "react-icons/fa";
 
 const DmChatContainerBox = ({ user2_id }) => {
   const { room_messages } = useSelector(({ roomsReducer }) => roomsReducer);
@@ -23,32 +24,77 @@ const DmChatContainerBox = ({ user2_id }) => {
   const actualUser =
     membersReducer && membersReducer.find((member) => member._id === user2_id);
   const user = actualUser ? actualUser : null;
-  console.log('This is the userooooo' + user?.name);
+
+  const messages = room_messages
+    ? room_messages.results.sort(
+        (a, b) => new Date(b.created_at) - new Date(a.created_at)
+      )
+    : [];
+
+  const groups = messages.reduce((groups, message) => {
+    const date = message.created_at.split("T")[0];
+    if (!groups[date]) {
+      groups[date] = [];
+    }
+    groups[date].push(message);
+
+    return groups;
+  }, {});
+
+  const groupedMessages = Object.keys(groups).map((date) => {
+    return {
+      date,
+      messages: groups[date],
+    };
+  });
+
+  console.log(groupedMessages);
+
+  //This function below helps group messages (Today, Yesterday etc)
+
+  const renderDate = (chat) => {
+    const timestampDate = new Date(chat).toDateString();
+
+    const todayDate = new Date().toDateString();
+
+    let prev_date = new Date();
+    prev_date.setDate(prev_date.getDate() - 1);
+
+    if (timestampDate === todayDate) {
+      return "Today";
+    } else if (new Date(prev_date).toDateString() == timestampDate) {
+      return "Yesterday";
+    }
+    return <>{new Date(chat).toUTCString().slice(0, 11)}</>;
+  };
+
   return (
     <>
-      <div className='dm-chatContainerBox w-100 d-flex align-items-end'>
-        <main className='dm-chat-main-container'>
-          {room_messages?.results
-            ? room_messages?.results
-                ?.sort((first, second) => {
-                  const firstDate = new Date(first.created_at);
-                  const secondDate = new Date(second.created_at);
-                  return secondDate - firstDate;
-                })
-                .map((messages) => (
-                  <div key={messages?.id}>
-                    <MessageWrapper handleOpenThread={handleOpenThread}>
-                      <DmSingleMessageContainer
-                        messages={messages}
-                        user2_id={user2_id}
-                      />
-                    </MessageWrapper>
-                  </div>
-                ))
-            : null}
+      <div className="dm-chatContainerBox w-100 d-flex align-items-end">
+        <main className="dm-chat-main-container">
+          {groupedMessages.length > 0
+            ? groupedMessages.map((group, index) => (
+                <span key={index}>
+                  <p className="__date_tag">
+                    {renderDate(group.date)} <FaAngleDown />
+                  </p>
+                  {group.messages.map((message, mIndex) => (
+                    <p key={mIndex}>
+                      <MessageWrapper handleOpenThread={handleOpenThread}>
+                        <DmSingleMessageContainer
+                          key={mIndex}
+                          messages={message}
+                          user2_id={user2_id}
+                        />
+                      </MessageWrapper>
+                    </p>
+                  ))}
+                </span>
+              ))
+            : ""}
           <DmInitMessageBox secondUser={user} />
         </main>
-        <aside className={`asideContent ${openThread ? 'active' : ''}`}>
+        <aside className={`asideContent ${openThread ? "active" : ""}`}>
           <DmReplyInThread handleCloseThread={handleCloseThread} />
         </aside>
       </div>
--- dmreactplugin/src/components/dmSingleMessageContainer.jsx
+++ dmreactplugin/src/components/dmSingleMessageContainer.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import '../assets/css/dmSingleMessageContainer.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import "../assets/css/dmSingleMessageContainer.css";
+import { FaAngleDown } from "react-icons/fa";
 
 function DmSingleMessageContainer({ messages, user2_id }) {
   const membersReducer = useSelector(({ membersReducer }) => membersReducer);
@@ -12,32 +13,53 @@ function DmSingleMessageContainer({ messages, user2_id }) {
   const messageTime = messages?.created_at;
   const localeTime = new Date(messageTime).toLocaleTimeString(
     {},
-    { hour: '2-digit', minute: '2-digit' }
+    { hour: "2-digit", minute: "2-digit" }
   );
 
+  // const dates = new Set();
+
+  // const renderDate = (chat, dateNum) => {
+  //   const timestampDate = new Date(chat.created_at).toDateString();
+
+  //   // Add to Set so it does not render again
+  //   dates.add(dateNum);
+  //   const todayDate = new Date().toDateString();
+
+  //   let prev_date = new Date();
+  //   prev_date.setDate(prev_date.getDate() - 1);
+
+  //   if (timestampDate === todayDate) {
+  //     return "Today";
+  //   } else if (new Date(prev_date).toDateString() == timestampDate) {
+  //     return "Yesterday";
+  //   }
+  //   return <>{timestampDate}</>;
+  // };
+
+  // const dateNum = new Date(messages.created_at).toDateString();
   return (
     <>
-      <div className='dm-plugin-thread-messages'>
-        <div className='dm-plugin-thread-message-group px-3 py-3'>
-          <div className='dm-plugin-thread-message-image-container'>
+      <div className="dm-plugin-thread-messages">
+        <div className="dm-plugin-thread-message-group px-3 py-3">
+          <div className="dm-plugin-thread-message-image-container">
             <img
               src={user?.image_url}
-              alt='Profile Picture'
-              width='36'
-              height='36'
-              className='dm-plugin-thread-message-image'
+              alt="Profile Picture"
+              width="36"
+              height="36"
+              className="dm-plugin-thread-message-image"
             />
           </div>
-          <div className='dm-plugin-thread-message-body'>
-            <p className='dm-plugin-thread-message-header'>
-              <span className='dm-plugin-thread-message-name'>
+          <div className="dm-plugin-thread-message-body">
+            <p className="dm-plugin-thread-message-header">
+              <span className="dm-plugin-thread-message-name">
                 {user?.user_name}
               </span>
-              <span className='dm-plugin-thread-message-time'>
+              <span className="dm-plugin-thread-message-time">
                 {localeTime}
               </span>
             </p>
-            <p className='dm-plugin-thread-message-text'>{messages?.message}</p>
+            <p className="dm-plugin-thread-message-text">{messages?.message}</p>
           </div>
         </div>
       </div>
