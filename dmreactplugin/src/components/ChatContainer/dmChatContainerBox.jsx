32a76f6f1976600f157d1fe65781c7d8a0f719f5
Wealth Iduwe <wealthiduwe@gmail.com>
2021-10-10 14:29:46 +0100

    profile poopup

--- dmreactplugin/src/components/ChatContainer/dmChatContainerBox.jsx
+++ dmreactplugin/src/components/ChatContainer/dmChatContainerBox.jsx
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import MessageWrapper from "../common/dmHoverState/dmHoverstate";
 import DmInitMessageBox from "../dmInitMessagebox";
 import DmSingleMessageContainer from "../dmSingleMessageContainer";
 import DmReplyInThread from "../ReplyInThread/replyInThread";
+import { RiArrowDownSLine } from "react-icons/ri";
+import centrifugeClient from "../../utils/centrifugoClient";
+
 import "./chatContainerBox.css";
 import { FaAngleDown } from "react-icons/fa";
 import { SubscribeToChannel } from "@zuri/control";
-import DmpopupProfile from "../DmPopupProfile";
 import { useDispatch } from "react-redux";
 import { handleGetRoomMessages } from "../../Redux/Actions/dmActions";
 
@@ -21,34 +23,27 @@ const DmChatContainerBox = ({
 }) => {
   const { room_messages } = useSelector(({ roomsReducer }) => roomsReducer);
   const [openThread, setOpenThread] = useState(false);
+
   const handleOpenThread = () => {
     setOpenThread(true);
   };
+
   const handleCloseThread = () => {
     setOpenThread(false);
   };
-  /* const dispatch = useDispatch()
-   useEffect(() => {
-    dispatch(handleGetRoomInfo(org_id, room_id))
-   }, [messages]) */
 
   const membersReducer = useSelector(({ membersReducer }) => membersReducer);
+
   const actualUser =
     membersReducer && membersReducer.find((member) => member._id === user2_id);
+
   const user = actualUser ? actualUser : null;
 
-  SubscribeToChannel(`${room_id}`, (ctx) => {
-    const websocket = ctx.data;
-    console.log("Websocket", websocket);
-    //console.log("This is centrifigo " + ctx);
-  });
-
-  const messages =
-    room_messages?.results.length > 0
-      ? room_messages.results.sort(
-          (a, b) => new Date(b.created_at) - new Date(a.created_at)
-        )
-      : [];
+  const messages = room_messages
+    ? room_messages.results.sort(
+        (a, b) => new Date(b.created_at) - new Date(a.created_at)
+      )
+    : [];
 
   const groups = messages.reduce((groups, message) => {
     const date = message.created_at.split("T")[0];
@@ -87,46 +82,45 @@ const DmChatContainerBox = ({
     return <>{new Date(chat).toUTCString().slice(0, 11)}</>;
   };
 
+  SubscribeToChannel(`${room_id}`, (ctx) => {
+    const websocket = ctx.data;
+    console.log("Websocket", websocket);
+    //console.log("This is centrifigo " + ctx);
+  });
+
   return (
     <>
       <div className="dm-chatContainerBox w-100 d-flex align-items-end">
         <main className="dm-chat-main-container">
-          <div key={messages?.id}>
-            <div className="__date_tags">
-              {groupedMessages.length > 0
-                ? groupedMessages.map((group, index) => (
-                    <span key={index}>
-                      <p className="__date_tag">
-                        {renderDate(group.date)} <FaAngleDown />
-                      </p>
-                      {group.messages.map((message, mIndex) => (
-                        <p key={mIndex}>
-                          <MessageWrapper handleOpenThread={handleOpenThread}>
-                            <DmSingleMessageContainer
-                              key={mIndex}
-                              messages={message}
-                              user2_id={user2_id}
-                            />
-                          </MessageWrapper>
-                        </p>
-                      ))}
-                    </span>
-                  ))
-                : ""}
-            </div>
-            <MessageWrapper handleOpenThread={handleOpenThread}>
-              <DmSingleMessageContainer
-                messages={messages}
-                user2_id={user2_id}
-                none={none}
-                setNone={setNone}
-                grid={grid}
-                setGrid={setGrid}
-                room_id={room_id}
-              />
-            </MessageWrapper>
+          {groupedMessages.length > 0
+            ? groupedMessages.map((group, index) => (
+                <span key={index}>
+                  <p className="__date_tag">
+                    {renderDate(group.date)} <FaAngleDown />
+                  </p>
+                  {group.messages.map((message, mIndex) => (
+                    <p key={mIndex}>
+                      <MessageWrapper handleOpenThread={handleOpenThread}>
+                        <DmSingleMessageContainer
+                          key={mIndex}
+                          messages={message}
+                          user2_id={user2_id}
+                          none={none}
+                          setNone={setNone}
+                          grid={grid}
+                          setGrid={setGrid}
+                          room_id={room_id}
+                        />
+                      </MessageWrapper>
+                    </p>
+                  ))}
+                </span>
+              ))
+            : ""}
+          <div className="dm-chat-main-container-sticky-date">
+            <span></span>
+            <RiArrowDownSLine />
           </div>
-          )
           <DmInitMessageBox secondUser={user} />
         </main>
         <aside className={`asideContent ${openThread ? "active" : ""}`}>
@@ -136,4 +130,5 @@ const DmChatContainerBox = ({
     </>
   );
 };
+
 export default DmChatContainerBox;
