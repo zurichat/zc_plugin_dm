51361bf1d61fb3afaea6c9e508e336b42edea8a2
Oluwaseye Owoborode <stevenowoborode@gmail.com>
2021-10-13 09:14:11 +0100

    Revert "Dev"

--- dmreactplugin/src/pages/newChatRoom.jsx
+++ dmreactplugin/src/pages/newChatRoom.jsx
@@ -1,40 +1,40 @@
-import React, { useEffect, useState, useRef } from 'react';
-import DmProfileHeader from '../components/dmProfileHeader';
-import BookmarkHeader from '../components/common/addBookmarkKebab/dmBookMark';
-import DmChatContainerBox from '../components/ChatContainer/dmChatContainerBox';
-import InputBoxField from '../components/dmBoxInputField';
-import PinnedMessage from '../components/common/pinnedMessage/dmPinnedMessages';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState, useRef } from 'react'
+import DmProfileHeader from '../components/dmProfileHeader'
+import BookmarkHeader from '../components/common/addBookmarkKebab/dmBookMark'
+import DmChatContainerBox from '../components/ChatContainer/dmChatContainerBox'
+import InputBoxField from '../components/dmBoxInputField'
+import PinnedMessage from '../components/common/pinnedMessage/dmPinnedMessages'
+import { useDispatch } from 'react-redux'
 import {
   handleGetRoomInfo,
   handleGetRoomMessages,
-} from '../Redux/Actions/dmActions';
-import { useSelector } from 'react-redux';
-import './newChatRoom.css';
-import ProfileSidebar from '../components/profileSidebar/profileSidebar';
+} from '../Redux/Actions/dmActions'
+import { useSelector } from 'react-redux'
+import './newChatRoom.css'
+import ProfileSidebar from '../components/profileSidebar/profileSidebar'
 
 // Chat Home Page
 const ChatHome = ({ org_id, loggedInUser_id, room_id }) => {
-  const roomsReducer = useSelector(({ roomsReducer }) => roomsReducer);
-  const membersReducer = useSelector(({ membersReducer }) => membersReducer);
-  const { room_messages } = useSelector(({ roomsReducer }) => roomsReducer);
+  const roomsReducer = useSelector(({ roomsReducer }) => roomsReducer)
+  const membersReducer = useSelector(({ membersReducer }) => membersReducer)
+  const { room_messages } = useSelector(({ roomsReducer }) => roomsReducer)
 
   const user2_id =
     roomsReducer?.room_info?.room_user_ids !== undefined &&
-    roomsReducer?.room_info?.room_user_ids[1];
-  const messages = room_messages?.results;
+    roomsReducer?.room_info?.room_user_ids[1]
+  const messages = room_messages?.results
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(handleGetRoomMessages(org_id, room_id));
-    dispatch(handleGetRoomInfo(org_id, room_id));
-  }, [dispatch, org_id, loggedInUser_id, room_id]);
+    dispatch(handleGetRoomMessages(org_id, room_id))
+    dispatch(handleGetRoomInfo(org_id, room_id))
+  }, [dispatch, org_id, loggedInUser_id, room_id])
 
-  const actualUser = membersReducer?.find((member) => member._id === user2_id);
+  const actualUser = membersReducer?.find((member) => member._id === user2_id)
 
-  const [grid, setGrid] = useState('');
-  const [none, setNone] = useState('none');
+  const [grid, setGrid] = useState('')
+  const [none, setNone] = useState('none')
 
   return (
     <div className='dm-plugin-full-page' style={{ display: grid }}>
@@ -60,10 +60,6 @@ const ChatHome = ({ org_id, loggedInUser_id, room_id }) => {
             user2_id={user2_id}
             room_id={room_id}
             actualUser={actualUser}
-            none={none}
-            setNone={setNone}
-            grid={grid}
-            setGrid={setGrid}
           />
         </div>
         <div className='dm-footer-input-field w-100 position-relative'>
@@ -84,6 +80,6 @@ const ChatHome = ({ org_id, loggedInUser_id, room_id }) => {
         />
       </div>
     </div>
-  );
-};
-export default ChatHome;
+  )
+}
+export default ChatHome
