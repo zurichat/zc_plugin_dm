b15f2377c74d81773ff603526e68c91033bc625f
xzenon <forlanfr_8@hotmail.fr>
2021-10-14 13:52:03 +0100

    Process the queue

--- backend/sync.py
+++ backend/sync.py
@@ -8,5 +8,21 @@ from drf_yasg.utils import swagger_auto_schema
 from .decorators import db_init_with_credentials
 
 
-async def sync_notifier():
-    print("json")
+@api_view(["GET", "POST"])
+def sync_notifier(request):
+    # Retrieve the queue for synchronization
+    queue = getQueue()
+    if not queue:
+        return Response(data="No data to update", status=status.HTTP_404_NOT_FOUND)
+
+    # Process if new data to update
+    last_queue_id = queue[len(queue) - 1]("id")
+
+    for data in queue:
+        if data["event"] == "enter_organization":
+            pass
+
+        if data["event"] == "leave_organization":
+            pass
+
+    return Response(data=request.data, status=status.HTTP_200_OK)
